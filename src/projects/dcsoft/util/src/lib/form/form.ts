569e0e08aef1c426b9e826d0657b36676af9064a
刘剑锋 <willow.lau@outlook.com>
2023-02-22 15:01:01 +0800

    更新树形表格提交刷新逻辑

--- src/projects/dcsoft/util/src/lib/form/form.ts
+++ src/projects/dcsoft/util/src/lib/form/form.ts
@@ -4,7 +4,7 @@
 //================================================
 import { HttpMethod } from "../http/http-method";
 import { Util } from "../util";
-import { AppConfig,initAppConfig } from "../config/app-config";
+import { AppConfig, initAppConfig } from "../config/app-config";
 import { IFormSubmitOptions } from "./form-submit-option";
 import { I18nKeys } from '../config/i18n-keys';
 
@@ -30,47 +30,47 @@ export class Form {
      * 提交表单
      * @param options 表单提交参数
      */
-    submit( options: IFormSubmitOptions ) {
-        if ( !this.validateSubmit( options ) )
+    submit(options: IFormSubmitOptions) {
+        if (!this.validateSubmit(options))
             return;
-        if ( !options.confirm ) {
-            this.submitForm( options );
+        if (!options.confirm) {
+            this.submitForm(options);
             return;
         }
-        this.util.message.confirm( {
+        this.util.message.confirm({
             title: options.confirmTitle,
             content: options.confirm,
-            onOk: () => this.submitForm( options ),
+            onOk: () => this.submitFormAsync( options ),
             onCancel: options.complete
-        } );
+        });
     }
 
     /**
      * 提交表单验证
      */
-    private validateSubmit( options: IFormSubmitOptions ) {
-        if ( !options ) {
-            console.log( "表单参数 options 未设置" );
+    private validateSubmit(options: IFormSubmitOptions) {
+        if (!options) {
+            console.log("表单参数 options 未设置");
             return false;
         }
-        if ( options.form && !options.form.valid ){
-            if(!options.form.valid){
-              Object.values(options.form.controls).forEach(control => {
-                if (control.invalid) {
-                  control.markAsDirty();
-                  control.updateValueAndValidity({ onlySelf: true });
-                }
-              });
-              return false;
+        if (options.form && !options.form.valid) {
+            if (!options.form.valid) {
+                Object.values(options.form.controls).forEach(control => {
+                    if (control.invalid) {
+                        control.markAsDirty();
+                        control.updateValueAndValidity({ onlySelf: true });
+                    }
+                });
+                return false;
             }
             return false;
         }
-        if ( !options.url ) {
-            console.log( "表单url未设置" );
+        if (!options.url) {
+            console.log("表单url未设置");
             return false;
         }
-        if ( !options.data ) {
-            console.log( "表单数据未设置" );
+        if (!options.data) {
+            console.log("表单数据未设置");
             return false;
         }
         return true;
@@ -79,32 +79,53 @@ export class Form {
     /**
      * 提交表单
      */
-    private submitForm( options: IFormSubmitOptions ) {
-        this.initHttpMethod( options );
-        this.util.webapi.send( options.url, options.httpMethod, options.data )
-            .header( options.header )
-            .button( options.button )
-            .loading( options.loading || false )
-            .handle( {
+    private submitForm(options: IFormSubmitOptions) {
+        this.initHttpMethod(options);
+        this.util.webapi.send(options.url, options.httpMethod, options.data)
+            .header(options.header)
+            .button(options.button)
+            .loading(options.loading || false)
+            .handle({
                 before: () => {
-                    return options.before && options.before( options.data );
+                    return options.before && options.before(options.data);
                 },
                 ok: result => {
-                    this.okHandler( options, result );
+                    this.okHandler(options, result);
                 },
                 fail: result => {
-                    this.failHandler( options, result );
+                    this.failHandler(options, result);
                 },
                 complete: options.complete
-            } );
+            });
+    }
+
+    /**
+     * 提交表单
+     */
+    private async submitFormAsync(options: IFormSubmitOptions) {
+        this.initHttpMethod(options);
+        await this.util.webapi.send(options.url, options.httpMethod, options.data)
+            .header(options.header)
+            .handleAsync({
+                before: () => {
+                    return options.before && options.before(options.data);
+                },
+                ok: result => {
+                    this.okHandler(options, result);
+                },
+                fail: result => {
+                    this.failHandler(options, result);
+                },
+                complete: options.complete
+            });
     }
 
     /**
      * 初始化Http方法
      * @param options
      */
-    private initHttpMethod( options: IFormSubmitOptions ) {
-        if ( options.httpMethod )
+    private initHttpMethod(options: IFormSubmitOptions) {
+        if (options.httpMethod)
             return;
         options.httpMethod = options.data.id ? HttpMethod.Put : HttpMethod.Post;
     }
@@ -112,24 +133,24 @@ export class Form {
     /**
      * 失败处理函数
      */
-    private failHandler( options: IFormSubmitOptions, result ) {
-        if ( options.form)
-            ( options.form as { submitted: boolean } ).submitted = false;
-        options.fail && options.fail( result );
-        if ( options.showErrorMessage !== false )
-            this.util.message.error( result.message );
+    private failHandler(options: IFormSubmitOptions, result) {
+        if (options.form)
+            (options.form as { submitted: boolean }).submitted = false;
+        options.fail && options.fail(result);
+        if (options.showErrorMessage !== false)
+            this.util.message.error(result.message);
     }
 
     /**
      * 成功处理函数
      */
-    private okHandler( options: IFormSubmitOptions, result ) {
-        options.ok && options.ok( result );
+    private okHandler(options: IFormSubmitOptions, result) {
+        options.ok && options.ok(result);
         if (options.showMessage !== false)
             this.util.message.success(options.message || I18nKeys.succeeded);
-        if ( options.back )
+        if (options.back)
             this.util.router.back();
-        if ( options.closeDialog ) {
+        if (options.closeDialog) {
             result = result || "ok";
             this.util.dialog.close(result);
         }
